aab267e813f4a83f5e783f427e1d2d636a7d064c
jeffzhangg <zhang.jeffreyd@gmail.com>
2016-09-17 19:01:10 -0400

    functionality with websocket handler

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,60 @@
 $(document).ready(function() {
   $('.landing__button').click(function() {
     event.preventDefault();
+    // querying username and group id
+      const username = $('.landing__username').val();
+      let groupID = $('.landing__group-id').val();
+      if (groupID === '') {
+        groupID = 'Group Chat';
+      }
     $('#landing').hide('easeInOutCubic');
     $('#chat').show();
-    $('body').css('overflow', 'hidden');
-    $('.chat__input-field__input').focus();
+    // setting body to overflow to remove double scrollbars
+      $('body').css('overflow', 'hidden');
+    // setting group name to specified group id
+      $('.chat__header__group-name').text(groupID);
+    // focusing input on section load
+      $('.chat__input-field__input').focus();
   });
 
   $('.chat__input-field__input').keyup(function(event) {
-    let message = $(this).val();
-    messageSubmit(event, message);
-  });
-
-  function messageSubmit(event, message) {
+    const message = $(this).val();
     // if browser supports event.keyCode, use so. if not, use event.which.
       const keyCode = (event.keyCode ? event.keyCode : event.which);
     // if the enter key was pressed, append message to chat, and clear input field.
       if (keyCode === 13) {
-        console.log(message);
-        if (message !== '') {
-          appendMessage(message);
-
-          let messageJSON = {
-            name: 'username',
-            msg: message
-          }
-          socket.send(JSON.stringify(messageJSON));
-        }
-
-        // clearing value of input
-          $('.chat__input-field__input').val('');
+        messageSubmit(event, message);
       }
+  });
+
+  function messageSubmit(event, message) {
+    console.log(message);
+    if (message !== '') {
+      appendMessage(message, 'client');
+
+      const messageJSON = {
+        name: 'username',
+        msg: message
+      }
+      socket.send(JSON.stringify(messageJSON));
+    }
+
+    // clearing value of input
+      $('.chat__input-field__input').val('');
   }
 
-  function appendMessage(message) {
-    let messageItem = document.createElement('li');
-    messageItem.innerText = message;
+  function appendMessage(message, type) {
+    const messageItem = document.createElement('div');
+    const li = document.createElement('li');
+    li.innerText = message;
+    messageItem.appendChild(li);
+    messageItem.className = type;
     $('.chat__messages__list').append(messageItem);
   }
 
   $('.chat__summary-toggle').click(function() {
     $('.chat__summary').toggle();
+    $('.chat__summary-toggle').toggleClass('chat__summary-toggle--black');
     $('#chat').toggleClass('chat--expanded-view');
   });
 
--- scripts/websocket.js
+++ scripts/websocket.js
@@ -2,15 +2,35 @@ const address = '104.198.12.154';
 const socket = new WebSocket('ws://' + address + '/chatroom/user');
 
 const message = {
-    name: 'user',
+    name: 'sumchat',
     msg: 'connected'
 };
 
 socket.onopen = function(event) {
-  console.log('connected');
-  socket.send(JSON.stringify('message'));
+  socket.send(JSON.stringify(message));
 }
 
+
 socket.onmessage = function(event) {
-  console.log(event.data)
+  let data = JSON.parse(event.data);
+  if (typeof currentUser === 'undefined') {
+    console.log('setting currentUser...');
+    let currentUser = data.name
+  }
+
+  if (currentUser === data.name) {
+    console.log('current user is still messaging, (dont update name)');
+  } else {
+    console.log('execute appendMessage fnc with special attribute that allows the injection of new name');
+  }
+  appendMessage(data.msg, 'server');
+}
+
+function appendMessage(message, type) {
+  const messageItem = document.createElement('div');
+  const li = document.createElement('li');
+  li.innerText = message;
+  messageItem.appendChild(li);
+  messageItem.className = type;
+  $('.chat__messages__list').append(messageItem);
 }
