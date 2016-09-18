8fdbcbe4204f344ad623f30fe6d95784725c9a73
jeffzhangg <zhang.jeffreyd@gmail.com>
2016-09-18 00:54:34 -0400

    users can now connect to websocket handler

--- scripts/websocket.js
+++ scripts/websocket.js
@@ -1,53 +1,53 @@
-var username;
-var group;
 const address = '104.198.12.154';
-let socket;
 
 var usernameCheck = setInterval(function() {
   if (getUsername() && getGroupID()) {
     clearInterval(usernameCheck);
-    username = getUsername();
-    group = getGroupID();
-    socket = new WebSocket('ws://' + address + '/' + group + '/' + username);
+    init();
   }
 }, 500);
 
+function init() {
+  var username = getUsername();
+  var group = getGroupID();
+  var socket = new WebSocket('ws://' + address + '/' + group + '/' + username);
 
-const message = {
-    name: 'sumchat',
-    msg: 'connected'
-};
+  const message = {
+      name: 'sumchat',
+      msg: 'connected'
+  };
 
-socket.onopen = function(event) {
-  console.log('connected');
-}
+  socket.onopen = function(event) {
+    console.log('connected');
+  }
 
-let currentUser;
-socket.onmessage = function(event) {
-  let data = JSON.parse(event.data);
-  let user = data.user;
-  // if current user doesn't exist then one is created
-    if (typeof currentUser === 'undefined') {
-      // currentUser is the user that websockets is sending
+  let currentUser;
+  socket.onmessage = function(event) {
+    let data = JSON.parse(event.data);
+    let user = data.user;
+    // if current user doesn't exist then one is created
+      if (typeof currentUser === 'undefined') {
+        // currentUser is the user that websockets is sending
+          currentUser = user;
+        console.log('current user:', currentUser);
+        if (username !== currentUser) {
+          appendMessage(data.msg, true, currentUser);
+        }
+      } else if (currentUser === user) {
+        console.log('same person is talking, dont change name');
+        if (username !== currentUser) {
+          appendMessage(data.msg, false);
+        }
+      } else {
         currentUser = user;
-      console.log('current user:', currentUser);
-      if (username !== currentUser) {
-        appendMessage(data.msg, true, currentUser);
+        console.log('diff person is talking, change the name');
+        if (username !== currentUser) {
+          appendMessage(data.msg, true, currentUser)
+        }
       }
-    } else if (currentUser === user) {
-      console.log('same person is talking, dont change name');
-      if (username !== currentUser) {
-        appendMessage(data.msg, false);
-      }
-    } else {
-      currentUser = user;
-      console.log('diff person is talking, change the name');
-      if (username !== currentUser) {
-        appendMessage(data.msg, true, currentUser)
-      }
-    }
-}
+  }
 
+}
 function appendMessage(message, nameBool, name) {
   const messageItem = document.createElement('div');
   if (nameBool) {
